c377c8637095d9c33134e26802d64e8f68f2b2b0
Gennady Kovshenin <gennady@kovshenin.com>
2017-02-07 18:46:19 +0500

    Ping

--- test.php
+++ test.php
@@ -4,6 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 final class TheTest extends TestCase {
 	public function testThisNow() {
+		var_dump( getenv( 'CAN_YOU_SEE_THIS' ) );
 		$this->assertTrue( true );
 	}
 }
